3db560b6651e02ced05608b8299606086dfc2943
Daniel Walton <dwalton76@gmail.com>
2020-01-11 19:57:27 -0500

    Added utils

---
+++ utils/spike-get-file.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2019 Daniel Walton <dwalton76@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+"""
+Get a micropython file to one of SPIKE's program slots
+"""
+
+import argparse
+import grp
+import logging
+import os
+import pwd
+import subprocess
+import sys
+
+
+log = logging.getLogger(__name__)
+
+
+def spike_get_file(dev: str, filename: str) -> bool:
+    """
+    'cat' the runtime.log file
+    """
+
+    # ampy requires root perms
+    if os.geteuid() != 0:
+        log.error("You must run this program using 'sudo'")
+        return False
+
+    if not os.path.exists(dev):
+        log.error(f"device '{dev}' is not connected")
+        return False
+
+    subprocess.check_output(f"ampy --port {dev} get {filename} > {filename}", shell=True)
+
+    username = os.environ["SUDO_USER"]
+    uid = pwd.getpwnam(username).pw_uid
+    gid = grp.getgrnam(username).gr_gid
+    os.chown(filename, uid, gid)
+
+    with open(filename, "r") as fh:
+        print(fh.read())
+
+    return True
+
+
+if __name__ == "__main__":
+
+    # configure logging
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(filename)16s %(levelname)8s: %(message)s")
+    log = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dev", type=str, default="/dev/ttyACM0", help="/dev/ttyXXXXX of SPIKE")
+    parser.add_argument("--filename", type=str, default="runtime.log")
+    args = parser.parse_args()
+
+    if not spike_get_file(args.dev, args.filename):
+        sys.exit(1)
---
+++ utils/spike-slots.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2019 Daniel Walton <dwalton76@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Print a table of the programs on SPIKE
+"""
+
+import argparse
+import logging
+import os
+import random
+import string
+import subprocess
+import sys
+
+try:
+    from tabulate import tabulate
+except (ImportError, ModuleNotFoundError) as e:
+    print("tabulate is not installed...run 'pip3 install tabulate'")
+    sys.exit(1)
+
+log = logging.getLogger(__name__)
+
+
+def indent(data: str, spaces: int) -> str:
+    """
+    Indent every line in 'data' by 'spaces'
+    """
+    result = []
+
+    for line in data.splitlines():
+        result.append(" " * spaces + line)
+
+    return "\n".join(result)
+
+
+def spike_slots(dev: str) -> str:
+    """
+    Return a table of the programs on SPIKE
+    """
+    SLOTS_FILENAME = "slots." + "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+    # ampy requires root perms
+    if os.geteuid() != 0:
+        log.error("You must run this program using 'sudo'")
+        return False
+
+    if not os.path.exists(dev):
+        log.error(f"device '{dev}' is not connected")
+        return False
+
+    # get the current .slots file
+    subprocess.check_output(f"ampy --port {dev} get projects/.slots > {SLOTS_FILENAME}", shell=True)
+
+    with open(SLOTS_FILENAME, "r") as fh:
+        slots = eval(fh.read())
+    os.unlink(SLOTS_FILENAME)
+
+    # build the table
+    rows = []
+    for slot_id in sorted(slots.keys()):
+        value = slots[slot_id]
+        rows.append([str(slot_id), value["name"], str(value["id"]), str(value["size"])])
+
+    table = tabulate(rows, headers=("slot", "description", "id", "size"))
+
+    # Add leading/trailing blank lines and indent the table by 2 spaces
+    print("\n" + indent(table, 2) + "\n")
+    return True
+
+
+if __name__ == "__main__":
+
+    # configure logging
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(filename)16s %(levelname)8s: %(message)s")
+    log = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dev", type=str, default="/dev/ttyACM0", help="/dev/ttyXXXXX of SPIKE")
+    args = parser.parse_args()
+
+    if not spike_slots(args.dev):
+        sys.exit(1)
