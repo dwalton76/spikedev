bd4d5d069c56e1280635cdcd2f0a573caee536fe
Daniel Walton <dwalton76@gmail.com>
2020-01-11 21:45:25 -0500

    Remove license header from individual files

--- utils/spike-slots.py
+++ utils/spike-slots.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python3
 
-# MIT License
-#
-# Copyright (c) 2019 Daniel Walton <dwalton76@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 """
 Print a table of the programs on SPIKE
 """
@@ -35,11 +13,8 @@ import string
 import subprocess
 import sys
 
-try:
-    from tabulate import tabulate
-except (ImportError, ModuleNotFoundError):
-    print("tabulate is not installed...run 'pip3 install tabulate'")
-    sys.exit(1)
+# third party libraries
+from tabulate import tabulate
 
 log = logging.getLogger(__name__)
 
