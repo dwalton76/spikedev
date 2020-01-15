90d2b07dccda8ede1bf4496743d3317bbef82707
Daniel Walton <dwalton76@gmail.com>
2020-01-14 22:34:15 -0500

    Added MoveSteering

--- spikedev/motor.py
+++ spikedev/motor.py
@@ -1,13 +1,37 @@
-# spikedev libraries
+# Much of the code in this file was ported from ev3dev-lang-python with minimal
+# changes...here is the license for ev3dev-lang-python.
+
+# -----------------------------------------------------------------------------
+# Copyright (c) 2015 Ralph Hempel <rhempel@hempeldesigngroup.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# -----------------------------------------------------------------------------
+
 # standard libraries
 import sys
-
 import utime
+
+# spikedev libraries
 from spikedev.logging import log_msg
 
 MAXINT = sys.maxsize
 MININT = MAXINT * -1
-
 
 BUSY_MODE = 0
 BUSY_SENSOR = 0
@@ -628,6 +652,42 @@ class MoveSteering(MoveTank):
         steering_drive.on_for_rotations(-20, SpeedPercent(75), 10)
     """
 
+    def get_speed_steering(self, steering, speed):
+        """
+        Calculate the speed_sp for each motor in a pair to achieve the specified
+        steering. Note that calling this function alone will not make the
+        motors move, it only calculates the speed. A run_* function must be called
+        afterwards to make the motors move.
+
+        steering [-100, 100]:
+            * -100 means turn left on the spot (right motor at 100% forward, left motor at 100% backward),
+            *  0   means drive in a straight line, and
+            *  100 means turn right on the spot (left motor at 100% forward, right motor at 100% backward).
+
+        speed:
+            The speed that should be applied to the outmost motor (the one
+            rotating faster). The speed of the other motor will be computed
+            automatically.
+        """
+
+        if steering < -100 or steering > 100:
+            raise ValueError("{} is an invalid steering, must be between -100 and 100 (inclusive)".format(steering))
+
+        # We don't have a good way to make this generic for the pair... so we
+        # assume that the left motor's speed stats are the same as the right
+        # motor's.
+        speed = self._speed_percentage(speed)
+        left_speed = speed
+        right_speed = speed
+        speed_factor = (50 - abs(float(steering))) / 50
+
+        if steering >= 0:
+            right_speed *= speed_factor
+        else:
+            left_speed *= speed_factor
+
+        return (left_speed, right_speed)
+
     def run_at_speed(self, steering, speed, max_power=0, acceleration=100, deceleration=150):
         """
         Start rotating the motors according to the provided ``steering`` and
@@ -665,39 +725,3 @@ class MoveSteering(MoveTank):
         """
         (left_speed, right_speed) = self.get_speed_steering(steering, speed)
         MoveTank.run_for_time(self, msec, left_speed, right_speed, max_power, stop, acceleration, deceleration, block)
-
-    def get_speed_steering(self, steering, speed):
-        """
-        Calculate the speed_sp for each motor in a pair to achieve the specified
-        steering. Note that calling this function alone will not make the
-        motors move, it only calculates the speed. A run_* function must be called
-        afterwards to make the motors move.
-
-        steering [-100, 100]:
-            * -100 means turn left on the spot (right motor at 100% forward, left motor at 100% backward),
-            *  0   means drive in a straight line, and
-            *  100 means turn right on the spot (left motor at 100% forward, right motor at 100% backward).
-
-        speed:
-            The speed that should be applied to the outmost motor (the one
-            rotating faster). The speed of the other motor will be computed
-            automatically.
-        """
-
-        if steering < -100 or steering > 100:
-            raise ValueError("{} is an invalid steering, must be between -100 and 100 (inclusive)".format(steering))
-
-        # We don't have a good way to make this generic for the pair... so we
-        # assume that the left motor's speed stats are the same as the right
-        # motor's.
-        speed = self._speed_percentage(speed)
-        left_speed = speed
-        right_speed = speed
-        speed_factor = (50 - abs(float(steering))) / 50
-
-        if steering >= 0:
-            right_speed *= speed_factor
-        else:
-            left_speed *= speed_factor
-
-        return (left_speed, right_speed)
