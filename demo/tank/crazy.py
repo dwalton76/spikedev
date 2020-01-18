9da896e59f76e57d54c45dc87bc223d29b3fa9ca
Daniel Walton <dwalton76@gmail.com>
2020-01-18 10:46:58 -0500

    added crazy.py

--- demo/tank/crazy.py
+++ demo/tank/crazy.py
@@ -4,7 +4,6 @@ import hub
 # spikedev libraries
 from spikedev.motor import SpikeMediumMotor
 # from spikedev.tank import MoveTank, MoveSteering
-# from spikedev.wheel import SpikeWheel
 
 mtr = SpikeMediumMotor(hub.port.E)
 mtr.run_for_degrees(360, 40)
